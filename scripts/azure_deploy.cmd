729bced80a4019d7745a4f48c632f8cc4e98744d
Tom <tom.clement@bizboard.nl>
2017-05-17 14:15:25 +0200

    Fixed server port, adding support for private bitbucket repositories

--- scripts/azure_deploy.cmd
+++ scripts/azure_deploy.cmd
@@ -61,6 +61,8 @@ IF !ERRORLEVEL! NEQ 0 goto error
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd %DEPLOYMENT_TARGET%
   call npm install
+  call npm config set strict-ssl false
+  call node node_modules/jspm/cli.js config registries.bitbucket.baseurl https://bitbucket.org/ && node node_modules/jspm/cli.js config registries.bitbucket.auth authbase64stringhere && node node_modules/jspm/cli.js config registries.bitbucket.version 1.0 && node node_modules/jspm/cli.js config registries.bitbucket.handler jspm-git
   call node node_modules/jspm/cli.js install
   call npm run build
   IF !ERRORLEVEL! NEQ 0 goto error
